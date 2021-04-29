9783bc90c06f4fcf0b61e5af0f994ff04f7297a5
cih-y2k <cih.2yk@gmail.com>
2021-04-29 17:27:52 +0700

    update message codes

--- protocol/message.go
+++ protocol/message.go
@@ -8,31 +8,31 @@ const (
 	/**
 	Close connection and destroy all handle routines
 	*/
-	MessageTypeClose = iota
+	MessageTypeClose
 	/**
 	Ping request message
 	*/
-	MessageTypePing = iota
+	MessageTypePing
 	/**
 	Pong response message
 	*/
-	MessageTypePong = iota
+	MessageTypePong
 	/**
 	Empty message
 	*/
-	MessageTypeEmpty = iota
+	MessageTypeEmpty
 	/**
 	Emit request, no response
 	*/
-	MessageTypeEmit = iota
+	MessageTypeEmit
 	/**
 	Emit request, wait for response (ack)
 	*/
-	MessageTypeAckRequest = iota
+	MessageTypeAckRequest
 	/**
 	ack response
 	*/
-	MessageTypeAckResponse = iota
+	MessageTypeAckResponse
 )
 
 type Message struct {
@@ -42,4 +42,3 @@ type Message struct {
 	Args   string
 	Source string
 }
-
